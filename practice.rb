00c19eaa2072e5e5c0b3951b7d21f36d95e49e12
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2018-10-22 15:35:45 -0700

    first commit

---
+++ practice.rb
@@ -0,0 +1,8 @@
+# string method practice
+my_str = "hello, my name is Andy"
+
+puts my_str.upcase
+puts my_str.reverse
+puts my_str.include?('n')
+puts my_str.capitalize
+puts "This is #{my_str} used in string interpolation"
